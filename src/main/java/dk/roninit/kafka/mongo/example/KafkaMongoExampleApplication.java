98718faecc6b430d193388649a5c83b448f5097c
Kasper Odgaard <kodga@nets.eu>
2024-11-06 08:48:51 +0100

    initial commit

---
+++ src/main/java/dk/roninit/kafka/mongo/example/KafkaMongoExampleApplication.java
@@ -0,0 +1,13 @@
+package dk.roninit.kafka.mongo.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaMongoExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaMongoExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dk/roninit/kafka/mongo/example/KafkaMongoExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.roninit.kafka.mongo.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaMongoExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
